1c4ecdc38bbbf83a2885d0eef8d37e57809b55d9
hamzah <hamzahnafalah@gmail.com>
2025-11-25 14:21:15 +0700

    update submodules

--- src/Controllers/API/Tenant/AddTenantController.php
+++ src/Controllers/API/Tenant/AddTenantController.php
@@ -5,13 +5,18 @@ namespace Projects\WellmedGateway\Controllers\API\Tenant;
 use Projects\WellmedGateway\Jobs\AddTenantJob;
 use Projects\WellmedGateway\Controllers\API\ApiController;
 use Illuminate\Http\Request;
-use Projects\WellmedBackbone\Jobs\JobRequest;
-use Symfony\Component\Process\Process;
 
 class AddTenantController extends ApiController{
     public function store(Request $request){
         try {
             $data = request()->all();
+            // $data = [
+            //     'workspace_id' => '01kawvy0cngm65jdqj61zvzpcb',
+            //     'workspace_name' => 'Wellmed h 9',
+            //     'product_label' => 'LITE',
+            //     'group_tenant_id' => 6,
+            //     'app_tenant_id' => 5
+            // ];
             dispatch(new AddTenantJob($data))->onQueue('installation')->onConnection('rabbitmq');
         } catch (\Throwable $th) {
             throw $th;
