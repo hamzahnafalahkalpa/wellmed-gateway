068bc441a5ccd31717996d3cc33bc5e61020d283
hamzah <hamzahnafalah@gmail.com>
2025-12-04 11:28:21 +0700

    update instalasi dan satu sehat

--- src/Controllers/API/Tenant/AddTenantController.php
+++ src/Controllers/API/Tenant/AddTenantController.php
@@ -10,21 +10,21 @@ class AddTenantController extends ApiController{
     public function store(Request $request){
         try {
             $data = request()->all();
-            // $data = [
-            //     'workspace_id' => '01kbk9kde1ap43xzmrw87qys8s',
-            //     'workspace_name' => 'Klinik Wellmed',
-            //     'product_label' => 'LITE',
-            //     'group_tenant_id' => 3,
-            //     'app_tenant_id' => 2,
-            //     'admin' => [
-            //         "id"=> null,
-            //         "name"=> "admin_wellmed",
-            //         "username"=> "admin_wellmed",
-            //         "email"=> "admin_wellmed@mail.com",
-            //         "password"=> "password",
-            //         "password_confirmation"=> "password"
-            //     ]
-            // ];
+            $data = [
+                "workspace_id" => "01kbks33epxgk8xxgbdbh0nqdq",
+                "workspace_name" => "Klinik Wellmed Kedua",
+                "product_label" => "LITE",
+                "app_tenant_id" => 2,
+                "group_tenant_id" => 3,
+                "admin" => [
+                    "id" => null,
+                    "name" => "admin_wellmed",
+                    "username" => "admin_wellmed",
+                    "email" => "admin_wellmed@mail.com",
+                    "password" => "password",
+                    "password_confirmation" => "password"
+                ]
+            ];
             dispatch(new AddTenantJob($data))->onQueue('installation')->onConnection('rabbitmq');
         } catch (\Throwable $th) {
             dd($th->getMessage());
